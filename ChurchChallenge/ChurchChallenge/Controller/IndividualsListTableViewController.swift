bcd0ca45bdb9fb1a5a5bd76cebf26879990b783e
Riley Hooper <ffhooper@gmail.com>
2018-04-30 12:50:08 -0600

    Added table view.

---
+++ ChurchChallenge/ChurchChallenge/Controller/IndividualsListTableViewController.swift
@@ -0,0 +1,56 @@
+//
+//  IndividualsListTableViewController.swift
+//  ChurchChallenge
+//
+//  Created by Riley Hooper on 4/30/18.
+//  Copyright © 2018 Riley Hooper. All rights reserved.
+//
+
+import UIKit
+
+class IndividualsListTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Uncomment the following line to preserve selection between presentations
+        self.clearsSelectionOnViewWillAppear = false
+        
+    }
+}
+
+extension IndividualsListTableViewController {
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 3
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? UITableViewCell
+        
+        cell?.textLabel?.text = "cell: \(indexPath.row)"
+        // Configure the cell...
+        
+        return cell!
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
